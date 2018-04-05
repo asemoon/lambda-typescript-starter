af61b4cb7b009932a0822dc9f5f232888147802a
Mehdi Karamenjad <me@mehdi.tech>
2018-04-05 16:00:00 -0700

    Temp changes to the out dir

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {Context} from "aws-lambda";
 
 const handler = (event: any, context: Context) => {
-    context.done(undefined, "Function is finished");
+    context.done(undefined, "Function is finished yes");
 };
 
 export {handler};
