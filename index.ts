d2173880fc64e267d9abd423d13b8b74bafbfb0a
Mehdi Karamenjad <me@mehdi.tech>
2018-04-05 15:05:56 -0700

    Adds AWS lambda types .

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-const BASE = "THIS IS THE BEGINNING";
+import {Context} from "aws-lambda";
+
+export default (event: any, context: Context) => {
+    context.done(undefined, "Called the job that removes expired events");
+};
