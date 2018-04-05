5a32afd373c8cb357e36278e9334aa77d392951f
Mehdi Karamenjad <me@mehdi.tech>
2018-04-05 16:05:20 -0700

    updates the out dir for typescript

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {Context} from "aws-lambda";
 
 const handler = (event: any, context: Context) => {
-    context.done(undefined, "Function is finished yes");
+    context.done(undefined, "Function is finished yes2");
 };
 
 export {handler};
