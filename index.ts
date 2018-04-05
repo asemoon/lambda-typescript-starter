1c4ac0660474f9df6f3a22957feeda1ae67334cb
Mehdi Karamenjad <me@mehdi.tech>
2018-04-05 15:22:57 -0700

    Updates the done message

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import {Context} from "aws-lambda";
 
 export default (event: any, context: Context) => {
-    context.done(undefined, "Called the job that removes expired events");
+    context.done(undefined, "Function is finished");
 };
