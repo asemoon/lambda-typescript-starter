3742741efe8c1b7381701c8650d80feba97c1bbc
Mehdi Karamenjad <me@mehdi.tech>
2019-04-02 12:00:09 -0700

    Updates the logging version and packages.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,22 @@
 import * as winston from "winston";
 
-const logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({timestamp: false, level: "silly"})
-    ]
+const { combine, timestamp, printf } = winston.format;
+
+const LOG_LEVEL = "debug";
+
+const customLogFormat = printf((info) => {
+    return `${info.timestamp} ${info.level}: ${info.message}`;
+});
+
+const transports = [new winston.transports.Console()];
+
+const logger = winston.createLogger({
+    level: LOG_LEVEL,
+    format: combine(
+        timestamp(),
+        customLogFormat
+    ),
+    transports
 });
 
 export default logger;
